b67adf4b53964d0896ac99c33568a4862764219f
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-06-19 17:06:00 -0300

    compilei o arquivo da lista

---
+++ listas/l0_refeita/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    //100 problemas de c.
+int main(void){
+    
+    printf("=======lista 0 refeita========");
+    //criação de menu para chamar as secções.
+    printf("ola mundo \n");
+
+
+    return(0);
+}
+void sec1(void){
+
+}
